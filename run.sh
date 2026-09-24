1b2cb4aec3be485b471a7d2e9a137db2f27ea929
Sumant-gituser <sumant@git.com>
2026-09-24 07:39:39 +0530

    commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-if [ -z ${1} ]; then
+if [ -e ${1} ]; then
     git pull 
     ansible-playbook -i ${1}-dev.sumantanil11.online, -e "ansible-user=ec2-user ansible-password=DevOps321" ${1}.yaml
 else
