1269b2d7c1f6f2c2bb53e5e7d451f6e51bbaf3ca
Sumant-gituser <sumant@git.com>
2026-09-24 07:34:50 +0530

    commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-if [ -z ${1} ];
-then
-    git pull; ansible-playbook -i ${1}-dev.sumantanil11.online, -e "ansible-user=ec2-user ansible-password=DevOps321" ${1}.yaml
-else:
+if [ -z ${1} ]; then
+    git pull; 
+    ansible-playbook -i ${1}-dev.sumantanil11.online, -e "ansible-user=ec2-user ansible-password=DevOps321" ${1}.yaml
+else
     echo parameter was empty
 fi
